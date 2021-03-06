a473a277174841c65f4eb01bef9de04ea102b245
lorsi96 <lorsi@itba.edu.ar>
2021-03-06 01:09:41 -0300

    Adds multiple sequences

--- src/program.c
+++ src/program.c
@@ -5,8 +5,9 @@
  * Date: 2020/12/30
  * Version: 0.1
  *===========================================================================*/
-#include "program.h"
+#include "lorsi.h"
 #include "sapi.h"
+#include <stdbool.h>
 
 /*=====[Program Constants]==========================*/
 
@@ -17,10 +18,51 @@
 #define MIN_ROLL_DELAY_MS 10
 #define MAX_ROLL_DELAY_MS 300
 
+#define GPIO_ARR_TO_SEQUENCE(arr) {arr, sizeof(arr)/sizeof(gpioMap_t) , 0}
+
+/** Led Sequences */
+typedef struct LedSequence {
+	gpioMap_t* ledSequence;
+	uint8_t numberOfLeds;
+	uint8_t ledCount;
+} LedSequence;
+
+gpioMap_t rgbSequence[] = {
+	LEDR, LEDG, LEDB
+};
+
+gpioMap_t nonRgbSequence[] = {
+	LED1, LED2, LED3
+};
+
+gpioMap_t invNonRgbSequence[] = {
+	LED3, LED2, LED1
+};
+
+typedef enum {
+	RGB,
+	NON_RGB_RIGHT,
+	NON_RGB_LEFT
+} Sequence;
+
+LedSequence allSequences[] = {
+	GPIO_ARR_TO_SEQUENCE(rgbSequence),
+	GPIO_ARR_TO_SEQUENCE(nonRgbSequence),
+	GPIO_ARR_TO_SEQUENCE(invNonRgbSequence)
+};
+
+LedSequence* sequences[] = {
+	&allSequences[RGB],
+	&allSequences[NON_RGB_RIGHT]
+};
+
+bool shiftRight = true;
+
 /** Rolling Led constants. */
 gpioMap_t LEDS_TO_ROLL[] = {
-   LED1, LED2, LED3, LEDB
+   LED1, LED2, LED3, LEDB, LEDG, LEDR
 };
+
 uint8_t LEDS_LEN = sizeof(LEDS_TO_ROLL) / sizeof(gpioMap_t);
 
 /*=====[Definition of the FSM ]==========================*/
@@ -47,9 +89,6 @@ uint8_t lfsr = 0b11001010;
 /** Time between LED blinks for the current sequence. */
 uint16_t currentRollDelay = MAX_ROLL_DELAY_MS;
 
-/** Rolling Direction. */
-static bool rollRight = true;
-
 /** Non-blocking delay managers. */
 static delay_t shortBlinkDelay;
 static delay_t longBlinkDelay; 
@@ -66,7 +105,12 @@ void doNothing() {}
 void errorCallback() {}
 
 void toggleDirection() {
-   rollRight = !rollRight;
+	shiftRight = !shiftRight;
+	if(shiftRight) {
+		sequences[1] = &allSequences[NON_RGB_RIGHT];
+	} else {
+		sequences[1] = &allSequences[NON_RGB_LEFT];
+	}
 }
 
 void startShortBlink() {
@@ -82,12 +126,11 @@ void startLongBlink() {
 void rollOnce() {
    static int8_t ledToRoll = 0;
    turnAllLedsOff_();
-   ledToRoll = (ledToRoll + 1) % ((int8_t)LEDS_LEN);
-   gpioWrite(
-      LEDS_TO_ROLL[
-         rollRight ? ledToRoll : (((int8_t)LEDS_LEN) - ledToRoll - 1)
-      ], 
-      ON);
+   uint8_t count;
+   for(count=0;count < sizeof(sequences)/sizeof(LedSequence*); count++) {
+	   gpioWrite(*(sequences[count]->ledSequence + sequences[count]->ledCount), ON);
+	   sequences[count]->ledCount = (sequences[count]->ledCount + 1) % sequences[count]->numberOfLeds;
+   }
 }
 
 void changeSpeedRandomly() {
@@ -135,6 +178,9 @@ Event getEvent() {
 /*=====[Private method definitions]==========================*/
 
 static void turnAllLeds_(uint8_t value) {
+   if(value != ON && value != OFF) {
+	   return;
+   }
    uint8_t cnt;
    for(cnt=0; cnt<LEDS_LEN; cnt++) {
       gpioWrite(LEDS_TO_ROLL[cnt], value);
